178cab49e179e5dd863be9c83476d65c42ab008f
MD Waliullah <waliullah9099@gmail.com>
2025-06-22 10:52:41 +0600

    createDate

--- my-time/main.go
+++ my-time/main.go
@@ -12,4 +12,8 @@ func main() {
 	fmt.Println(presentTime)
 
 	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday"))
+
+	createDate := time.Date(2005, time.April, 10, 3, 7, 17, 8, time.UTC)
+	fmt.Println(createDate.Format("01-02-2006 15:04:05 Monday"))
+
 }
