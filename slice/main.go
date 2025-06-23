a138709810e011931f65417dcd13f5dd189b888f
MD Waliullah <waliullah9099@gmail.com>
2025-06-23 10:26:54 +0600

    var index int = 2

--- slice/main.go
+++ slice/main.go
@@ -2,39 +2,51 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
-	var fruitsList = []string{"Appel", "Banana"}
+	/*
+		var fruitsList = []string{"Appel", "Banana"}
 
-	fmt.Printf("Type of the fruitsList: %T\n", fruitsList)
+		fmt.Printf("Type of the fruitsList: %T\n", fruitsList)
 
-	fruitsList = append(fruitsList, "Tomato", "mango")
+		fruitsList = append(fruitsList, "Tomato", "mango")
 
-	fmt.Println(fruitsList)
+		fmt.Println(fruitsList)
 
-	fruitsList = append(fruitsList[1:])
-	fmt.Println(fruitsList)
+		fruitsList = append(fruitsList[1:])
+		fmt.Println(fruitsList)
 
-	highScores := make([]int, 4)
+		highScores := make([]int, 4)
 
-	highScores[0] = 343
-	highScores[1] = 234
-	highScores[2] = 324
-	highScores[3] = 543
-	// highScores[4] = 111
+		highScores[0] = 343
+		highScores[1] = 234
+		highScores[2] = 324
+		highScores[3] = 543
+		// highScores[4] = 111
 
-	highScores = append(highScores, 121, 32, 54)
+		highScores = append(highScores, 121, 32, 54)
 
-	fmt.Println(highScores)
+		fmt.Println(highScores)
 
-	fmt.Println(sort.IntsAreSorted(highScores))
+			fmt.Println(sort.IntsAreSorted(highScores))
+			sort.Ints(highScores)
 
-	sort.Ints(highScores)
+			fmt.Println(highScores)
 
-	fmt.Println(highScores)
+			fmt.Println(sort.IntsAreSorted(highScores))
+	*/
 
-	fmt.Println(sort.IntsAreSorted(highScores))
+	// how to remove a value from slice based on index
+
+	var courses = []string{"react.js", "next.js", "typescript", "php", "java", "golang"}
+
+	fmt.Println(courses)
+
+	var index int = 2
+
+	courses = append(courses[:index], courses[index+2:]...)
+
+	fmt.Println(courses)
 
 }
